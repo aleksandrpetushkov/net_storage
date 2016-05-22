f614fbe126d4f50079498a8d21458adeffba509b
Alexander Petushkov <aleks.petushkov@gmail.com>
2016-05-22 13:40:16 +0600

    0.0.1

---
+++ net_storage/net_storage_srv/main.cpp
@@ -0,0 +1,24 @@
+#include <SFML\Network.hpp>
+#include <iostream>
+
+
+
+using namespace std;
+using namespace sf;
+
+int main()
+{
+	char k;
+	cout << "HW\n";
+	TcpListener lst;
+	lst.listen(154);
+	TcpSocket t_sc_lst;
+	
+
+
+
+	cin.get();
+	
+}
+
+
